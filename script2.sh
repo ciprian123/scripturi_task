4e241e46285bff8cc71a3fc4284d8357285bbc7d
Ciprian Ursulean <ciprian.ursulean5@gmail.com>
2021-02-05 15:35:57 +0200

    Verificare numar necesar de argumente la linia de comanda

--- script2.sh
+++ script2.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# this script automates several comands: shebang, touch, chmod and vim. 
+if [  "$#" -ne 1 ]; then 
+	echo "this script requires one argument wich is the script name"
+exit 1 
+fi
+
 # dam un folder ca parametru si din el stergem fisierele goale
 if [ ! -d "$1" ]; then
     echo "Directorul nu exista!"
