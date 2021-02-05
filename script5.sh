40a02ba436720689ebac8f0b394398af7245ac99
Ciprian Ursulean <ciprian.ursulean5@gmail.com>
2021-02-05 15:32:57 +0200

    Verificare numar de argumente la executie

--- script5.sh
+++ script5.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 # creez fisierul dat ca parametru, ii dau drepturi de executare si ii adaug shebang-ul
+# this script automates several comands: shebang, touch, chmod and vim. 
+if [  "$#" -ne 1 ]; then 
+	echo "this script requires one argument wich is the script name"
+exit 1 
+fi
+
 touch $1
 chmod +x $1
 echo "#!/bin/bash" > $1
